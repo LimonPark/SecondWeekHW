4f1c0328d0e8b54370ab79f12e0acbfeb3a156e2
nikita <kafes-nik@mail.ru>
2022-09-28 02:04:19 +0300

    Second week hw

--- src/ClassesWithAttributes/ProductManager.java
+++ src/ClassesWithAttributes/ProductManager.java
@@ -3,7 +3,7 @@ package ClassesWithAttributes;
 public class ProductManager {
     public void Add(Product product){
         //JDBC
-        System.out.println("Urun eklendi " + product.name);
+        System.out.println("Urun eklendi " + product.getName());
     }
 
     public void Add2(int id, String name, String description, int stockAmount, double price, String yeniOzellik){
